df964a64a10f7e8ed1242f06786934a10c454806
maxfurt <marcoruckl75@Hotmail.com>
2023-06-17 16:40:02 -0400

    Atualizando bd

--- Pessoa_Front-End/src/app/page.js
+++ Pessoa_Front-End/src/app/page.js
@@ -10,16 +10,19 @@ export default async function Home() {
 
   return (
     <main> <Link href="/cadastro" className='voltar'> CADASTRAR </Link>
-
+<div className="grid grid-cols-4 ml-28 mr-28 gap-4 mt-10 ">
       {produtos.map(produtos => (
+       <div className="bg-slate-400 border w-full  h-full border-blue-900"> 
         <div key={produtos.codigo}>
-        <img src={produtos.imagem}/>
+        <img src={produtos.imagem} style={{ width: '150px', height: '150px' }}/>
         <p>{produtos.titulo}</p>
         <p>{produtos.descrição}</p>
         <p>{produtos.data_cadastro}</p>
           <Link href={`/produto/${produtos.id}`}>ver mais</Link>
         </div>
+        </div>
       ))}
+      </div>
     </main>
   )
 }
